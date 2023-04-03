eed7e0cb72fdf1bb39dbd9e8149731bc810fb832
Eben2023 <ebenpresec@yahoo.com>
2023-04-03 15:40:28 -0700

    update

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,34 +2,46 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - finds the loop in a linked list
- * @head: linked list to search for
+ * find_linked_list_loop - Finds the start of a loop in a linked list
+ * @head: Pointer to the head node of the linked list
  *
- * Return: address of the node where the loop starts, or NULL
+ * Returns: The address of the node where the loop starts, or NULL if there is no loop
  */
-listint_t *find_listint_loop(listint_t *head)
-{
-listint_t *slow = head;
-listint_t *fast = head;
 
+listint_t *find_linked_list_loop(listint_t *head)
+{
+// Use two pointers to traverse the linked list
+listint_t *slow_ptr = head;
+listint_t *fast_ptr = head;
+
+// If the linked list is empty
 if (!head)
-return (NULL);
+return NULL;
 
-while (slow && fast && fast->next)
+// Traverse the linked list until the pointers meet or fast_ptr reaches the end
+while (slow_ptr && fast_ptr && fast_ptr->next)
 {
-fast = fast->next->next;
-slow = slow->next;
-if (fast == slow)
+fast_ptr = fast_ptr->next->next;
+slow_ptr = slow_ptr->next;
+
+// If the pointers meet, a loop is detected
+if (fast_ptr == slow_ptr)
 {
-slow = head;
-while (slow != fast)
+// Move slow_ptr to the start of the linked list
+slow_ptr = head;
+
+// Traverse both pointers until they meet at the loop start
+while (slow_ptr != fast_ptr)
 {
-slow = slow->next;
-fast = fast->next;
+slow_ptr = slow_ptr->next;
+fast_ptr = fast_ptr->next;
 }
-return (fast);
+
+// Return the loop start address
+return fast_ptr;
 }
 }
 
-return (NULL);
+// No loop detected
+return NULL;
 }
