2a35441c7bad17708a0423acfb2da736a72d1405
Eben2023 <ebenpresec@yahoo.com>
2023-04-09 15:56:15 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,4 @@
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "main.h"
 
 /**
  * read_textfile - Reads a text file then prints it to POSIX standard output
@@ -12,41 +10,41 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-ssize_t bytes_read, bytes_written;
 char *buffer;
+int file_descriptor;
+ssize_t byte_read;
+ssize_t byte_written;
 
-if (filename == NULL)
+file_descriptor = open(filename, O_RDONLY);
+if (file_descriptor == -1)
+{
 return (0);
-
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
-
+}
 buffer = malloc(sizeof(char) * letters);
 if (buffer == NULL)
 {
-close(fd);
+close(file_descriptor);
 return (0);
 }
-
-bytes_read = read(fd, buffer, letters);
-if (bytes_read == -1)
+if (filename == NULL)
 {
-free(buffer);
-close(fd);
 return (0);
 }
-
-bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-if (bytes_written == -1 || bytes_written != bytes_read)
+byte_read = read(file_descriptor, buffer, letters);
+if (byte_read == -1)
 {
+close(file_descriptor);
 free(buffer);
-close(fd);
 return (0);
 }
-
+byte_written = write(STDOUT_FILENO, buffer, byte_read);
+if (byte_written == -1 || byte_written != byte_read)
+{
+close(file_descriptor);
 free(buffer);
-close(fd);
-return (bytes_read);
+return (0);
+}
+close(file_descriptor);
+free(buffer);
+return (byte_read);
 }
