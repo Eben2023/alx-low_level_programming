e64a8514575ac2c46b374ca5b2a21fa043233ce2
Eben2023 <ebenpresec@yahoo.com>
2023-04-09 16:53:13 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,30 +10,35 @@
 
 int create_file(const char *filename, char *text_content)
 {
-int fd, bytes_written, length = 0;
+
+int file_length = 0;
+int byte_written;
+int file_descriptor;
 
 if (filename == NULL)
 return (-1);
 
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-if (fd == -1)
+file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (file_descriptor == -1)
 return (-1);
 
-if (text_content != NULL)
+if (!(text_content == NULL))
 {
 
-while (text_content[length] != '\0')
-length++;
-
-bytes_written = write(fd, text_content, length);
-if (bytes_written == -1 || bytes_written != length)
+for (int a = 0; text_content[i] != '\0'; a++)
 {
-close(fd);
+file_length++;
+}
+
+byte_written = write(file_descriptor, text_content, file_length);
+if (byte_written == -1 || byte_written != file_length)
+{
+close(file_descriptor);
 return (-1);
 }
 }
 
-close(fd);
+close(file_descriptor);
 return (1);
 }
 
