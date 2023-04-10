7f8e70a2ee4e1980c8b4388a85fdcbf275d890ad
Eben2023 <ebenpresec@yahoo.com>
2023-04-09 17:32:07 -0700

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,27 +10,29 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fd;
-int nletters;
-int rwr;
+int fd, res, len = 0;
 
-if (!filename)
+if (filename == NULL)
 return (-1);
 
-fd = open(filename, O_WRONLY | O_APPEND);
+if (text_content != NULL)
+{
+while (text_content[len])
+len++;
+}
 
+fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
 
-if (text_content)
+if (len > 0)
 {
-for (nletters = 0; text_content[nletters]; nletters++)
-;
-
-rwr = write(fd, text_content, nletters);
-
-if (rwr == -1)
+res = write(fd, text_content, len);
+if (res != len)
+{
+close(fd);
 return (-1);
+}
 }
 
 close(fd);
