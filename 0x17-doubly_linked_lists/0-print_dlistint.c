f5f7692f6d8d03f4d712cd739f67b17fb302d71b
Eben2023 <ebenprec@yahoo.com>
2023-05-18 16:40:09 -0700

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,28 +1,15 @@
 #include "lists.h"
 
-size_t print_dlistint(const dlistint_t *h);
-
-int main(void)
+size_t print_dlistint(const dlistint_t *h)
 {
-dlistint_t *head;
-dlistint_t *new;
-dlistint_t hello = {8, NULL, NULL};
-size_t n;
+size_t count = 0;
 
-head = &hello;
-new = malloc(sizeof(dlistint_t));
-if (new == NULL)
+while (h != NULL)
 {
-dprintf(2, "Error: Can't malloc\n");
-return (EXIT_FAILURE);
+printf("%d\n", h->n);
+h = h->next;
+count++;
 }
-new->n = 9;
-head->prev = new;
-new->next = head;
-new->prev = NULL;
-head = new;
-n = print_dlistint(head);
-printf("-> %lu elements\n", n);
-free(new);
-return (EXIT_SUCCESS);
+
+return (count);
 }
