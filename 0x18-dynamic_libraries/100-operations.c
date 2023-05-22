a39a9a9f1296788cb0a10d4aa64503c4ffc2b2bf
Eben2023 <ebenprec@yahoo.com>
2023-05-22 12:20:22 -0700

    update

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+/**
+ * add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of the two integers.
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * sub - Subtracts two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The difference between the two integers.
+ */
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * mul - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The product of the two integers.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * div - Divides two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of integer division.
+ */
+int div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * mod - Computes the modulus of two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The modulus of the two integers.
+ */
+int mod(int a, int b)
+{
+return (a % b);
+}
