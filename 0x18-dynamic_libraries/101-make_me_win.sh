8102a30d2ddfe9695f41c1387699e17d53bdaae3
Eben2023 <ebenprec@yahoo.com>
2023-05-22 13:20:14 -0700

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo -e '#include <stdio.h>\nunsigned long int strtoul(const char *nptr, char **endptr, int base) { return 1; }' > fake_strtoul.c
-gcc -shared -o fake_strtoul.so -fPIC fake_strtoul.c
-LD_PRELOAD=./fake_strtoul.so ./gm 9 8 10 24 75 9
+cp /bin/true /tmp/gm
+chmod u+x /tmp/gm
