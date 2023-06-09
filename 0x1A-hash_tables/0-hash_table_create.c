bf4886e299a56208906fea4c5846f597646a4285
Eben2023 <ebenprec@yahoo.com>
2023-06-09 05:30:10 -0700

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,14 +15,14 @@ unsigned long int i;
 /* Allocate memory for the hash table structure */
 hash_table = malloc(sizeof(hash_table_t));
 if (hash_table == NULL)
-return NULL;
+return (NULL);
 
 /* Allocate memory for the array of hash nodes */
 hash_table->array = malloc(sizeof(hash_node_t *) * size);
 if (hash_table->array == NULL)
 {
 free(hash_table);
-return NULL;
+return (NULL);
 }
 
 /* Initialize each element of the array to NULL */
@@ -31,5 +31,5 @@ hash_table->array[i] = NULL;
 
 hash_table->size = size;
 
-return hash_table;
+return (hash_table);
 }
